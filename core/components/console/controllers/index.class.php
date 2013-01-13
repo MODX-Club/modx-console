93bc5208cb67d837497508ec0c6a150cfa659881
proxyfabio <proxyfabio@gmail.com>
2013-01-13 07:52:54 +0400

    Ace fix

--- core/components/console/controllers/index.class.php
+++ core/components/console/controllers/index.class.php
@@ -3,10 +3,12 @@
 class ConsoleIndexManagerController extends ConsoleManagerController{
     
     function process(array $scriptProperties = array()) {
+
+        #add js
+        $this->addJavascript($this->config['assets_url'].'js/mgr/widgets/console.panel.js');
+        $this->addJavascript($this->config['assets_url'].'js/mgr/sections/home.js');
+
         $this->modx->invokeEvent('OnSnipFormPrerender');
-        return array(
-            "config" => $this->modx->toJSON($this->config),
-        );
     }
     
     function getTemplateFile() {
