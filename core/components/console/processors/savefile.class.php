fc44bb2781b63fabd0cf70ef30af2b11bc96a13f
sergant210 <sergant210@bk.ru>
2015-09-03 13:37:21 +0300

    Save code to file or snippet

--- core/components/console/processors/savefile.class.php
+++ core/components/console/processors/savefile.class.php
@@ -3,7 +3,7 @@
 class ConsoleSaveFileProcessor extends modProcessor{
     public function process() {
         $code = trim($this->getProperty('code',''));
-        $fileName = trim($this->getProperty('name',''));
+        $fileName = basename(trim($this->getProperty('name','')));
         $path = $this->modx->getOption('console_core_path', NULL, $this->modx->getOption('core_path') . 'components/console/').'files/';
         if (!is_dir($path) && !mkdir($path,0755)) $this->failure($this->modx->lexicon('console_err_path_nf'));
         if (!empty($code)) file_put_contents($path.$fileName.'.php', $code );
