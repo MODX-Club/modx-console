a3038f54634d178045cb8ef4b3f2a3b75c555bad
sergant210 <sergant210@bk.ru>
2015-09-03 13:48:43 +0300

    Edit savefile processor

--- core/components/console/processors/savefile.class.php
+++ core/components/console/processors/savefile.class.php
@@ -9,7 +9,7 @@ class ConsoleSaveFileProcessor extends modProcessor{
         $fileName = basename(trim($this->getProperty('name','')));
         $path = $this->modx->getOption('console_core_path', NULL, $this->modx->getOption('core_path') . 'components/console/').'files/';
         if (!is_dir($path) && !mkdir($path,0755)) $this->failure($this->modx->lexicon('console_err_path_nf'));
-        if (!empty($code)) file_put_contents($path.$fileName.'.php', $code );
+        if (!empty($code) && $fileName) file_put_contents($path.$fileName.'.php', $code );
 
         return $this->success();
     }
